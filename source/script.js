d665db0bf55c993c285e38894c2d21da9f567ef9
Fabiano Júnior Lima Barbosa <138934847+Fabito02@users.noreply.github.com>
2024-07-27 16:51:36 -0300

    Add files via upload

---
+++ source/script.js
@@ -0,0 +1,16 @@
+function primeiraParte() {
+    document.getElementById('primeiraParte').scrollIntoView({ behavior: 'smooth' });
+}
+
+function segundaParte() {
+    document.getElementById('segundaParte').scrollIntoView({ behavior: 'smooth' });
+}
+
+function terceiraParte() {
+    document.getElementById('terceiraParte').scrollIntoView({ behavior: 'smooth' });
+}
+
+function quartaParte() {
+    document.getElementById('quartaParte').scrollIntoView({ behavior: 'smooth' });
+}
+
