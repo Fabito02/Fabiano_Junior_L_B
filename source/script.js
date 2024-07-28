a62ce4b105739ee46000545d2bcef89551c5dc2b
Fabiano Júnior Lima Barbosa <138934847+Fabito02@users.noreply.github.com>
2024-07-28 15:03:08 -0300

    Add files via upload

--- source/script.js
+++ source/script.js
@@ -14,48 +14,25 @@ function quartaParte() {
     document.getElementById('quartaParte').scrollIntoView({ behavior: 'smooth' });
 }
 
-function img0() {
-    let modal = document.getElementById('modal');
-    let modalImg = document.getElementById('modalImg');
-    let corpo = document.querySelector('main')
-    modalImg.src = "img/img0.png"
-    modal.style.display = 'flex'
-    corpo.style.filter = 'blur(7px)'
-}
+var modal = document.getElementById('modal');
+var modalImg = document.getElementById('modalImg');
+var corpo = document.querySelector('main')
 
-function img1() {
-    let modal = document.getElementById('modal');
-    let modalImg = document.getElementById('modalImg');
-    let corpo = document.querySelector('main')
-    modalImg.src = "img/img1.png"
-    modal.style.display = 'flex'
-    corpo.style.filter = 'blur(7px)'
-}
-
-function img2() {
-    let modal = document.getElementById('modal');
-    let modalImg = document.getElementById('modalImg');
-    let corpo = document.querySelector('main')
-    modalImg.src = "img/img2.png"
-    modal.style.display = 'flex'
-    corpo.style.filter = 'blur(7px)'
-}
-
-function img3() {
-    let modal = document.getElementById('modal');
-    let modalImg = document.getElementById('modalImg');
-    let corpo = document.querySelector('main')
-    modalImg.src = "img/img3.png"
+function openModal(image) {
+    modalImg.src = image.src
     modal.style.display = 'flex'
     corpo.style.filter = 'blur(7px)'
 }
 
 function closeModal() {
-    let modal = document.getElementById('modal');
-    let modalImg = document.getElementById('modalImg');
-    let corpo = document.querySelector('main')
     modal.style.display = 'none'
     corpo.style.filter = 'blur(0px)'
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
 }
 
 
