4e04a131478a1380af9dd47f8d13b0464fd24375
Fabito02 <fabianojuniorlimaba2@gmail.com>
2024-07-27 22:01:48 -0300

    Corigido bugs, interface melhorada, adicionado o favicon.png

--- source/script.js
+++ source/script.js
@@ -14,3 +14,48 @@ function quartaParte() {
     document.getElementById('quartaParte').scrollIntoView({ behavior: 'smooth' });
 }
 
+function img0() {
+    let modal = document.getElementById('modal');
+    let modalImg = document.getElementById('modalImg');
+    let corpo = document.querySelector('main')
+    modalImg.src = "img/img0.png"
+    modal.style.display = 'flex'
+    corpo.style.filter = 'blur(7px)'
+}
+
+function img1() {
+    let modal = document.getElementById('modal');
+    let modalImg = document.getElementById('modalImg');
+    let corpo = document.querySelector('main')
+    modalImg.src = "img/img1.png"
+    modal.style.display = 'flex'
+    corpo.style.filter = 'blur(7px)'
+}
+
+function img2() {
+    let modal = document.getElementById('modal');
+    let modalImg = document.getElementById('modalImg');
+    let corpo = document.querySelector('main')
+    modalImg.src = "img/img2.png"
+    modal.style.display = 'flex'
+    corpo.style.filter = 'blur(7px)'
+}
+
+function img3() {
+    let modal = document.getElementById('modal');
+    let modalImg = document.getElementById('modalImg');
+    let corpo = document.querySelector('main')
+    modalImg.src = "img/img3.png"
+    modal.style.display = 'flex'
+    corpo.style.filter = 'blur(7px)'
+}
+
+function closeModal() {
+    let modal = document.getElementById('modal');
+    let modalImg = document.getElementById('modalImg');
+    let corpo = document.querySelector('main')
+    modal.style.display = 'none'
+    corpo.style.filter = 'blur(0px)'
+}
+
+
